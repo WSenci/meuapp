01fe17ce6988e9786408380e775a25f8f926182b
Wesley Senciani <wsa.wes05@gmail.com>
2024-05-03 20:03:47 -0300

    fazendo adicionar task

--- src/components/topoTask/index.tsx
+++ src/components/topoTask/index.tsx
@@ -1,39 +1,34 @@
 import { Box, Button, Center, Checkbox, Input, Spacer, Text } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
-import { Tarefa } from "../../interfaces/tarefa";
+import api from "../../helpers/axios";
 
 interface Props {
-    tasks: Tarefa[]
-    setTasks(tasks: Tarefa[]): void
+    carregarLista(): void
 }
 
-function TopoTask({ tasks, setTasks }: Props) {
+function TopoTask({ carregarLista }: Props) {
     const [nomeTask, setNomeTask] = useState('')
 
     function adicionarTask() {
-        if (tasks.length > 0) {
-            const ultimoId = tasks[tasks.length - 1].id
-            const novaTask = {
-                id: ultimoId + 1,
+        if (nomeTask != '') {
+            const novaTarefa = {
                 title: nomeTask,
                 completed: false
             }
-
-            setTasks([...tasks, novaTask])
+            api.post('/task', novaTarefa)
+                .then(() => {
+                    setNomeTask('')
+                    carregarLista()
+                })
         }
     }
-
-    const inputTask = useRef<HTMLInputElement | null>(null)
-    useEffect(()=>{
-        if(inputTask.current) inputTask.current.focus()
-    }, [])
 
     return (
         <>
             <Text fontSize='35px' margin='5px'>Inserir nova tarefa:</Text>
             <Box bg='white' w='50%' p='4' color='black' display='flex' gap={5}>
                 <Center w='50%'>
-                    <Input ref={inputTask} value={nomeTask} onChange={(evento) => setNomeTask(evento.target.value)} placeholder="Digite o título da task" w='100%' />
+                    <Input value={nomeTask} onChange={(evento) => setNomeTask(evento.target.value)} placeholder="Digite o título da task" w='100%' />
                 </Center>
                 <Spacer />
                 <Center>
